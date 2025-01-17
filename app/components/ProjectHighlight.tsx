169a5526ec404a5ebc3b2a5393b1faee49d9c03e
haldenl <halden.lin@gmail.com>
2025-01-17 09:31:14 -0800

    nit mt for mobile 2nd image w/ no title

--- app/components/ProjectHighlight.tsx
+++ app/components/ProjectHighlight.tsx
@@ -73,7 +73,7 @@ const ProjectHighlight: React.FC<ProjectHighlightProps> = ({
           <a
             href={img2Meta?.link}
             target="_blank"
-            className={`mt-12 z-10 md:mt-0 pointer-cursor md:w-[90%] h-auto transition-transform md:z-20 group-hover/imgs:z-40 md:translate-x-[10%] md:-translate-y-[5%] xl:group-hover/imgs:scale-110 md:group-hover/imgs:scale-[70%] xl:group-hover/imgs:translate-x-[30%] md:group-hover/imgs:translate-x-[25%] xl:group-hover/imgs:-translate-y-[20%] md:group-hover/imgs:-translate-y-[20%] xl:group-hover/imgs:rotate-6 xl:group-hover/imgs:duration-200 duration-50 ${hasImg2Title ? "md:group-hover/imgs:brightness-75" : ""} hover:!brightness-100 peer-hover:!brightness-100`}
+            className={`${hasImg2Title ? 'mt-12' : 'mt-6'} z-10 md:mt-0 pointer-cursor md:w-[90%] h-auto transition-transform md:z-20 group-hover/imgs:z-40 md:translate-x-[10%] md:-translate-y-[5%] xl:group-hover/imgs:scale-110 md:group-hover/imgs:scale-[70%] xl:group-hover/imgs:translate-x-[30%] md:group-hover/imgs:translate-x-[25%] xl:group-hover/imgs:-translate-y-[20%] md:group-hover/imgs:-translate-y-[20%] xl:group-hover/imgs:rotate-6 xl:group-hover/imgs:duration-200 duration-50 ${hasImg2Title ? "md:group-hover/imgs:brightness-75" : ""} hover:!brightness-100 peer-hover:!brightness-100`}
           >
             {hasImg2Title && (
               <div className="pointer-events-none mb-2 transition-all duration-200 w-full flex flex-row justify-center md:opacity-0 md:group-hover/imgs:opacity-100">
